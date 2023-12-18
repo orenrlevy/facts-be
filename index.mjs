a17451135d11adff3b168058a5d304b21853d8f4
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-18 18:49:18 +0200

    first

--- index.mjs
+++ index.mjs
@@ -1,11 +1,8 @@
-import OpenAI from "openai";
-import express from 'express';
-
 export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify('Hello from Lambda!2'),
   };
   return response;
 };

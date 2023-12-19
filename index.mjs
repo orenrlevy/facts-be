2ec63fef255b69254d7ccf48dfc4371dc3e7b837
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-19 15:20:08 +0200

    open ai

--- index.mjs
+++ index.mjs
@@ -1,9 +1,36 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_SECRET_KEY
+});
+
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!2'),
+  let response = {
+    statusCode: null,
+    body: null,
   };
+  try {
+    console.log("\nTheory: " + request.body.theory)
+
+    const completion = await openai.chat.completions.create({
+        messages: [{"role": "system", "content": "Assume the role of a fact-checker tasked with assessing the validity of the claims presented in the following text. Write a one-paragraph analysis comparing the key points in the text to established knowledge on the subject matter, highlighting any inconsistencies or contradictions between the text's assertions and widely recognized information. Text: ###"},
+            {"role": "user", "content": request.body.theory}],
+        model: "gpt-4-1106-preview",
+    });
+
+    console.log("\nFact: " + completion.choices[0].message.content);
+
+    response.body = completion.choices[0].message.content;
+    response.statusCode = 200;
+
+  } catch (error) {
+    console.log("\nError:");
+    console.log(error);
+
+    response.body = error;
+    response.statusCode = 500;
+  }
+  
   return response;
 };
 

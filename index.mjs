9d013097d4e3e81b3e49b2921563874ceed4964b
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-20 10:21:18 +0200

    ready v1

--- index.mjs
+++ index.mjs
@@ -9,28 +9,19 @@ export const handler = async (event) => {
     statusCode: null,
     body: null,
   };
-
-  console.log("\nInput:");
-  console.log(event);
-
   try {
     console.log("\nTheory: " + event.body)
-
     const completion = await openai.chat.completions.create({
         messages: [{"role": "system", "content": "Assume the role of a fact-checker tasked with assessing the validity of the claims presented in the following text. Write a one-paragraph analysis comparing the key points in the text to established knowledge on the subject matter, highlighting any inconsistencies or contradictions between the text's assertions and widely recognized information. Text: ###"},
             {"role": "user", "content": event.body}],
         model: "gpt-4-1106-preview",
     });
-
     console.log("\nFact: " + completion.choices[0].message.content);
-
     response.statusCode = 200;
     response.body = completion.choices[0].message.content;
     return response;
   } catch (error) {
-    console.log("\nError:");
-    console.log(error);
-    
+    console.log("\nError: " + error);
     response.statusCode = 500;
     response.message = error;
     return response;

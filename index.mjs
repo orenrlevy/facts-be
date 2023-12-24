454d30285d94317a9fac0edb78f65de1bcd6c500
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-24 14:59:13 +0200

    new prompt

--- index.mjs
+++ index.mjs
@@ -17,25 +17,35 @@ let tavily = {
   "exclude_domains": []
 };
 
-const promptPrefix = `Assume the role of a fact-checker tasked with assessing the validity of the claims presented in the following text. 
-                      If the assertions in the text can only be verified by training on data post April 2023 add “training on data post April 2023 is critical for verification” at the end of your output. 
-                      Write an analysis comparing the key points in the text to established knowledge on the subject matter, highlighting any inconsistencies or contradictions between the text's assertions and widely recognized information. 
-                      I am going to provide a template for writing your analysis. 
-                      CAPITALIZED WORDS are my placeholders for content. 
-                      Try to fit the output into one or more of the placeholders that I list. 
-                      Please preserve the formatting and overall template that I provide below.
-                      Use the below template twice. 
-                      In the first instance, use a fact checker personality in your writing style. 
-                      In the second instance, use John Oliver's style. 
-                      Seperate the two styles with "%%%SEPERATOR%%%" and don't provide titles of the personality type.
+const promptPrefix = `
+  Assume the role of a fact-checker tasked with assessing the validity of the claims presented in the following text. 
+  If the assertions in the text are likely to be influenced by data post April 2023 add “training on data post April 2023 is critical for verification” per the template provided below. Information that typically requires post April 2023 data includes recent events, updated statistics, or new scientific research. You can also use key indicators to determine if the text might be referencing events or developments beyond April 2023. Key indicators may include on of the following five indicators:
+  1. Specific Dates or Time References: If the text mentions a specific date or time period that is after April 2023, this is a clear indicator that the information requires post April 2023 data.
+  2. Recent Events or Trends: References to "recent events," "current trends," or phrases like "as of this year" can suggest that the information requires post April 2023 data.
+  3. Advancements in Technology or Science: Mentions of new technologies, scientific discoveries, or advancements that you are not aware of can indicate they emerged after April 2023.
+  4. World Events and News: If a text talks about major world events, political changes, or news stories that you don't recognize or that seem to have developed beyond your knowledge, this suggests they are recent and require post April 2023 data.
+  5. Cultural and Social Changes: References to new cultural phenomena, social movements, gossip or public figures that have risen to prominence recently can also indicate a post-training development which requires post April 2023 data.
 
-                      This is the template: 
-                        TLDR - here you will provide the bottom line verdict of your analysis. 
-                        It can be as short as one word such as “true” or “false” or as long as one sentence. 
-                        X - here you will write your analysis in a format that fits X (previously known as Twitter) limits. 
-                        Hence this summary will be no longer than 280 characters. 
-                        Always start with the question: “VeReally?
-                        ”SUMMARY - here you will write your analysis in one detailed paragraph.`;
+  If the assertions in the text are not likely to be affected by data post April 2023, do not add “training on data post April 2023 is critical for verification” in the template provided below.
+
+  Write an analysis comparing the key points in the text to established knowledge on the subject matter, highlighting any inconsistencies or contradictions between the text's assertions and widely recognized information. 
+
+  Identify the language of the provided text. Provide your response in the same language as the provided text.
+
+  I am going to provide a template for writing your analysis. CAPITALIZED WORDS are my placeholders for content. Try to fit the output into one or more of the placeholders that I list. Please preserve the formatting and overall template that I provide below. 
+
+  Use the below template twice. In the first instance, use a fact checker personality in your writing style. In the second instance, use John Oliver's style. Seperate the two styles with "%%%SEPERATOR%%%". Do not provide titles of the personality type.Thus, both instances will start with the TLDR section per the template below. 
+
+  This is the template: 
+
+  TLDR - here you will provide the bottom line verdict of your analysis. It can be as short as one word such as “true” or “false” or as long as one sentence. 
+
+  X - here you will write your analysis in a format that fits X (previously known as Twitter) limits. Hence this summary will be no longer than 280 characters. Always start with the question: “VeReally?” as the first word after “X”.
+
+  SUMMARY - here you will write your analysis in one detailed paragraph. 
+
+  CURRENT EVENTS - include this section only if the assertions in the text are likely to be influenced by data post April 2023. If the assertions in the text are likely to be influenced by data post April 2023, write only one sentence in English: “training on data post April 2023 is critical for verification” in this section. Do not translate it even if the provided text is not in English. Information that  typically requires post-April 2023 data includes recent events, updated statistics, or new scientific research. If the assertions in the text are not likely to be affected by data post April 2023, do not include this section and do not write anything here. So, do not include the 'CURRENT EVENTS' section unless there are clear indicators based on your training data that it's necessary.
+  `;
 
 const inputPrefix = " Text: ### ";
 const inputSuffix = " ###";

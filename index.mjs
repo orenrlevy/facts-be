adbce2f4006884db1f0d949d1be2757f633fdf05
Yotam Kadishay <kadishay.yotam@gmail.com>
2024-01-09 19:40:36 +0200

    logging advanced

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import * as https from 'https';
 import {promptPrefix, promptPrefixTavili, promptFormatter, promptSummarize, promptSupport} from './prompts.mjs';
 import zlib from 'zlib';
 import logger from './logger';
+import aws from 'aws-sdk';
 
 /*
 const openai = new OpenAI({
@@ -365,3 +366,34 @@ function makeRequest(host, path, method, body, pathParams, headers, extractGzip)
     req.end();
   });
 }
+
+
+//https://adelachao.medium.com/aws-sending-cloudwatch-custom-logs-from-lambda-node-js-e0379ea7a34c
+
+const logger = async (message) => {
+    const cloudwatchlogs = new aws.CloudWatchLogs();
+
+    // describeLogStreams to get sequenceToken
+    const describeParams = {
+        limit: 1,
+        logGroupName: "fact-checker",
+        logStreamNamePrefix: "test"
+    }
+
+    const res = await cloudwatchlogs.describeLogStreams(describeParams).promise();
+    const logStreams = res.logStreams;
+    const sequenceToken = logStreams[0].uploadSequenceToken;
+
+    // putLogEvents 
+    const putLogParams = {
+    logEvents: [{
+        message: JSON.stringify(message),
+        timestamp: new Date().getTime()
+        }],
+        logGroupName: "fact-checker",
+        logStreamName: "fact-checker",
+        sequenceToken
+    };
+
+    return await cloudwatchlogs.putLogEvents(putLogParams).promise();
+};
